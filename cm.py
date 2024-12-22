abcbc0c509ade35965ac89093dcd6775e25a1de5
Rohit Chand <119827556+The-Kind-Sage@users.noreply.github.com>
2024-12-22 12:42:09 +0545

    Add files via upload
    There is also .txt file in my this repository , This is an updated version of that with some few changes.  This script works on dictionary method to save data in terminal/console. 
    
    Working Description :
    
    It has feature to "Add Contact" which ask user to enter their name, and their phone number which is  added to computer memory as dictionary. There is another feature which is "Update contact", it ask user to enter name so that it can search that particular contact, after that it ask new number to replace the existing number. Furthermore, there is "Delete contact" option, like wise "Update contact" it also ask user to enter name so that it can search that particular contact, then it delete the existing contact. Next, it has "View Contact" option to see all  the recently added contact , Unlike recent options it doesn't ask  user to input anything and lastly, this program include an option to close program which is "Exit". It break my loop to close my program.
    
    I had too much fun to create this program. 
    Hello, World!

---
+++ cm.py
@@ -0,0 +1,56 @@
+print ("\n\n\n [   ~  WELCOME TO CONTACT MANAGER  ~   ] \n ****************************************")
+print ("\n Dear user,\n You have different options to begin with. ")
+print("\n \n Options:\n -------\n ")
+print(" 1. Add Contact\n 2. Update Contact \n 3. Delete Contact\n 4. View Contacts\n 5. Exit")
+   
+contacts={}
+
+while True:
+    z=input(" \n\n Kindly Enter Your Choice: ")
+    
+    if z=="1":
+            
+            name=input("Enter Contact Name: ")
+            no=int(input("Enter Contact Number: "))
+            contacts[name] = no
+            print(f"\nContact '{name}' added successfully! \n")
+    elif z=="2":
+            
+                updt=input("Enter the name of the contact to update:")
+                if updt in contacts:
+                    ncn=int(input("Enter new contact number: "))
+                    contacts[updt] = ncn
+                                   
+                    print(f"\n Contact '{name}' updated  successfully! \n")
+            
+                else:
+                    print(f"\n No contact found with the name '{updt}'.")
+            
+    elif z=="3":
+             
+                deln=input("Enter the name of the contact to delete:")
+            
+                if deln in contacts:
+                    del contacts[deln]
+                    print(f"\n Contact '{name}' deleted successfully! \n")
+            
+                else:
+                    print(f"\nNo contact found with the name '{deln}'.")
+            
+    elif z=="4":
+        if contacts:
+              print("\n Your Contacts: " )
+              print(" --------------\n")
+              for name, no in contacts.items():
+                  print(f"[ Name:{name}, Phone No:{no} ]")
+        else:
+            print("You have no contacts")
+            
+    elif z=="5":
+        print("\n Exiting Contact Manager, Have a wonderful day ahead :)")
+        print(" ***************************************************")
+        break
+    else:
+        print("\n Invalid Choice. You only have options between 1 to 5.")     
+        
+
